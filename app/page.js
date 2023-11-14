a73174d5a35e41823a451ce235d7e1ce0396e4ad
darkohedgehog <zivic.darko79@gmail.com>
2023-11-14 20:11:28 +0100

    Modified Files

--- app/page.js
+++ app/page.js
@@ -1,10 +1,21 @@
-import HomeCover from "./home/page";
+import ParticlesContainer from './components/ParticlesContainer';
+import MainHeader from './components/MainHeader';
+import MainServices from './components/MainServices';
+import WorkSlider from './components/WorkSlider';
+import Clients from './components/Clients';
+import MainCarousel from './components/MainCarousel';
 
 
 export default function Home() {
-  return (
-   <>    
-   <HomeCover />   
-   </>
+  return (     
+   <div className='relative'>
+      <ParticlesContainer />
+      <MainHeader />      
+      <MainServices />      
+      <WorkSlider />  
+      <Clients />   
+      <MainCarousel />                   
+    </div>   
+   
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,7 @@ module.exports = {
       lg: "960px",
       xl: "1200px",
     },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          explosion: 'url("/bg-explosion.png")',
-      },
+    extend: {     
       colors: {
         primary: "#131424",
         secondary: "#393A47",
