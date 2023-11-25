a0362dd74e6e77fbd2a75c3231067f8a61acb9a9
darkohedgehog <zivic.darko79@gmail.com>
2023-11-25 22:14:42 +0100

    Modified file

--- app/terms/page.jsx
+++ app/terms/page.jsx
@@ -3,74 +3,71 @@ const Terms = () => {
   return (
     <>
     
-    <div className="container">
-<p>
-    <h3>Uslovi korišćenja ovog web sajta:</h3>
-     <h4>Ovaj web sajt se može koristiti pod sledećim uslovima:</h4>
+    <div className="flex items-center justify-center mx-10 py-28 mt-20">
+<div className="text-gray-300" >
+    <h2 className="text-bold text-gray-200 mb-10 text-3xl">Uslovi korišćenja ovog <span className="text-accent mx-1">web</span>  sajta:</h2>
+     <h4 className="flex items-center justify-center my-10 text-gray-300 text-lg">Ovaj web sajt se može koristiti pod sledećim uslovima:</h4>
 
-<p>
-Autorsko pravo
-Sva autorska prava za sadržaj i publikacije na ovom web sajtu poseduje s.z.f.r. `Dizajn`
-</p>
+<div className="mb-5">
+<h3 className="mb-2 text-lg text-accent">Autorsko pravo</h3>
+Sva autorska prava za sadržaj i publikacije na ovom web sajtu poseduje s.z.f.r. `Dizajn`.
+</div>
 
-<p>
+<div className="mb-5">
 Korišćenjem bilo kog dela ovog web sajta, prihvatate pravila njegovog korišćenja. Korišćenjem sadržaja bilo koje stranice ovog web sajta smatra se da ste upoznati sa ovim pravilima.
-</p>
+</div>
 
-<p>
+<div className="mb-5">
 Možete preuzeti i privremeno sačuvati jednu ili više stranica ovog web sajta za potrebe pregledanja. Takođe možete štampati bilo koju publikaciju sa ovog web sajta dokle god je svaka kopija kompletna kopija, i nijedna izmena nije napravljena.
-</p>
+</div>
 
-<p>
+<div className="mb-5">
 Svako drugo skladištenje, kopiranje, prenos ili distribucija sadržaja ili publikacija sa ovog web sajta je zabranjeno bez saglasnosti s.z.f.r. `Dizajn` i biće predmet sudskog gonjenja.
-</p>
+</div>
 
-<p>
+<div className="mb-5">
 s.z.f.r. `Dizajn` upozorava da je nezakonito i zabranjeno bilo kakva neovlašćena izmena podataka na ovom web sajtu, njihovo brisanje ili uništenje, činjenje nedostupnim ili namerno izazivanje štete,  preduzeti pravnu akciju radi naknade prouzrokovane štete.
-</p>
-<p>
+</div>
+<div className="mb-5">
 s.z.f.r. `Dizajn` može, bez ikakve prethodne najave, da privremeno ili trajno izmeni ili ukine ovaj web sajt u celini ili pojedine delove.
-</p>
-<p>
+</div>
+<div className="mb-5">
 s.z.f.r. `Dizajn` nije odgovoran za bilo kakvu direktnu ili indirektnu, slučajnu materijalu ili nematerijalnu štetu, gubitke ili troškove nastale korišćenjem ili nemogućnošću da se koriste sve ili neke od informacija dostupnih na ovom web sajtu.
-</p>
-<h4>Linkovanje</h4>
-<p>
+</div>
+<h4 className="mb-5 text-accent text-lg">Linkovanje</h4>
+<div className="mb-5">
 Linkovanje sa ovog web sajta je zabranjeno bez saglasnosti s.z.f.r. `Dizajn`.
-</p>
-<p>
+</div>
+<div className="mb-5">
 Svaki napor je preduzet da se osigura tačnost informacija na ovom web sajtu, kao i internet linkova koji se nalaze na ovom web sajtu.
-</p>
-<p>
+</div>
+<div className="mb-5">
 Međutim, zbog prirode medija i rizika od prekida i poremećaja, bilo koja odgovornost koje bi mogla nastati iz upotrebe ili oslanjanja na podatke ili linkove koji se nalaze na ovom web sajtu je isključena.
-</p>
-<p>
+</div>
+<div className="mb-5">
 Mi ne garantujemo niti prihvatamo bilo kakvu odgovornost za bilo koji sadržaj na ili iz softvera preuzetog sa bilo kog web sajta linkovanim sa ovim sajtom.
-</p>
-<h4>Informacije</h4>
-<p>
+</div>
+<h4 className="mb-5 text-accent text-lg">Informacije</h4>
+<div className="mb-5">
 Informacije koje su dostupne na ovom web sajtu predstavljaju samo opšte informacije.
-</p>
-<p>
+</div>
+<div className="mb-5">
 Za detaljne informacije molimo vas da stupite u kontakt sa s.z.f.r. `Dizajn` na sledeću adresu:
-</p>
-<p>
-Za detaljne informacije molimo vas da stupite u kontakt sa s.z.f.r. `Dizajn` na sledeću adresu:
-</p>
-<p>
+</div>
+<div className="mb-5">
 s.z.f.r. `Dizajn`
-ul. Glavaševa 3
-11420 Smederevska Palanka, Republika Srbija
-mob: + 381 64 1706 370
+ul. Žikice Damjanovića 15,
+11420 Smederevska Palanka, Republika Srbija,
+mob: + 381 64 1706 370,
 e-mail: nino05dizajn@gmail.com
-</p>
+</div>
 
-<p>
+<div className="mb-5">
 Važeći zakon
 Ovo pravno obaveštenje kao i sva pitanja u vezi ovog web sajta regulisani su zakonom Republike Srbije.
-</p>
+</div>
 
-</p>
+</div>
     </div>
     
     </>
